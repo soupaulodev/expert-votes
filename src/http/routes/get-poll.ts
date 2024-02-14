0634cb15f2f6e6c0d55a05cf469a6634429b4089
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-02-14 00:21:18 -0300

    Add getPoll route

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from "fastify"
+import z from "zod"
+import { prisma } from "../../lib/prisma"
+
+export async function getPoll(app: FastifyInstance) {
+    app.get('/polls/:pollId', async (request, reply) => {
+        const getPollParams = z.object({
+            pollId: z.string().uuid(),
+        })
+
+        const { pollId } = getPollParams.parse(request.params)
+
+        const poll = await prisma.poll.findUnique({
+            where: {
+                id: pollId,
+            }
+        })
+    
+        return reply.send({ poll })
+    })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from 'fastify';
 import { createPoll } from './routes/create-poll';
+import { getPoll } from './routes/get-poll';
 
 const app = fastify()
 const appPort = 5000
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: appPort }).then(() => {
     console.log(`
