cf206adc254f2e4bd4df220f7249fec4fb7f507f
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-02-14 23:36:42 -0300

    add vote ranking register

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { randomUUID } from 'node:crypto';
 import { prisma } from '../../lib/prisma';
 import { FastifyInstance } from 'fastify';
+import { redis } from '../../lib/redis';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -35,6 +36,8 @@ export async function voteOnPoll(app: FastifyInstance) {
           }
         })
 
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({ message: 'You have already voted on this poll' })
       }
@@ -58,6 +61,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       }
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return reply.status(201).send();
   });
