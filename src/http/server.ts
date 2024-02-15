10fcbdcef94e5a26b0521a6f2933e552dedf5f30
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-02-15 01:52:45 -0300

    Create route poll results and implement realtime with websocket

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
+import cookie from '@fastify/cookie';
+import websocket from '@fastify/websocket';
 import { fastify } from 'fastify';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
-import cookie from '@fastify/cookie';
+import { pollResults } from './ws/poll-results';
 
 const app = fastify()
 const appPort = 5000
@@ -12,9 +14,12 @@ app.register(cookie, {
     hook: "onRequest",
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: appPort }).then(() => {
     console.log(`
